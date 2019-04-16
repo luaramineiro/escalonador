85c935168ea0507c80eccf0bf94a8cdfe5b6c6db
Luara Mineiro <luaramineiro@gmail.com>
2019-04-16 17:54:14 -0300

    firts commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# SISTEMAS OPERACIONAIS - ESCALONAMENTO DE PROCESSOS
+
+## Como executar
+Para executar o programa só abrir o arquivo index.html em um browser
+
+O usuário deverá informar qual algoritimo deverá ser usado no escalonamento. Logo após indicar o número de processos, os tempos de entrada de cada processo, e o tempo de uso da cpu de cada processo.
+
+-FIFO (Não-Preemptivo)
+-SJF (Não-Preemptivo)
+-Round Robin (Preemptivo)
+-EDF (Preemptivo)
+
+Os algoritmos Preemptivos irão pedir ao usuário o quantum de processamento, e terão uma sobrecarga de preempção de 1 unidade de tempo. O algoritmo EDF, além de pedir o quantum ao usuário, irá pedir tambem as deadlines dos processos para a simulação da execução.
+Os resultados que os algoritmos irão retornar, serãos os Turnaround Médio da execução.
+
+## Licença
+Copyright (c) 2019 Liliane Fiuza e Luara Mineiro
+Licensed under the MIT license.
---
+++ js/index.js
@@ -0,0 +1,50 @@
+// Variável global da lista de processos
+var processos = [];
+
+$(document.body).ready(function () {
+    $('#myChart').hide();
+    $('#turnaround').hide();
+
+    // Limpa a tabela dos processos, o gráfico e lista de processos
+    $('#btn-limpar').click(function (e) {
+        $('#tabela-processos').html('');
+        $('#myChart').hide();
+        $('#turnaround').hide();
+        processos = [];
+    });
+
+    $('#btn-adicionar').click(function (e) {
+        e.preventDefault();
+        var execucao = $('#execucao').val();
+        var chegada = $('#chegada').val();
+        var deadline = $('#deadline').val();
+        var pid = Math.round(Math.random() * (99999 - 1000) + 1000) + "";
+
+        // Adiciona o processo a tabela
+        $('#tabela-processos').append('<tr> <td id="processoNome' +
+            pid + '">' + pid + '</td> <td>' +
+            chegada + '</td><td>' +
+            execucao + '</td><td>' +
+            deadline + '</td> <td></td>  </tr>');
+
+        var processo = new Object();
+        processo['pid'] = parseInt(pid);
+        processo['execucao'] = parseInt(execucao);
+        processo['chegada'] = parseInt(chegada);
+        processo['deadline'] = parseInt(deadline);
+        processo['falta_executar'] = parseInt(execucao);
+
+        // Adiciona o processo a lista de processos
+        processos.push(processo);
+
+        $('#modalAdicionar').modal('hide');
+
+        limparCamposModal();
+    });
+});
+
+function limparCamposModal() {
+    $('#execucao').val('');
+    $('#chegada').val('');
+    $('#deadline').val('');
+}
